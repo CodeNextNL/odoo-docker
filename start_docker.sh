4ddd8eac4138dfca2a0c4231e9f3cb48fcb3d853
wytzejan <wjriedstra@gmail.com>
2019-11-01 15:11:09 +0100

    restart docker container if already running

--- start_docker.sh
+++ start_docker.sh
@@ -3,4 +3,10 @@
 DEPLOYMENT_SERVER_USER=$1
 cd /home || exit
 cd $DEPLOYMENT_SERVER_USER || exit
-docker-compose up -d
+if [ -z "$(docker-compose ps -q odoo)" ] || [ -z "$(docker ps -q --no-trunc | grep "$(docker-compose ps -q odoo)")" ]; then
+  # not running
+  docker-compose up -d
+else
+  # already running, restart to activate changes
+  docker-compose restart
+fi
